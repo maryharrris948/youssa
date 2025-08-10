94cb73a5755e99ad3e83e60583ef88d1349a30c2
sunshine-web-coder <maryharrris948@gmail.com>
2025-08-10 23:21:00 +0100

    fff

--- src/components/mockData/MockData.ts
+++ src/components/mockData/MockData.ts
@@ -406,7 +406,7 @@ export const mockAccounts: Account[] = [
     },
     bank_details: {
       account_type: 'Checking',
-      balance_usd: 48801470.0
+      balance_usd: 50801470.0
     },
     transaction_mgs_code: {
       transaction_text_msg: 'To continue this transaction, please enter the code sent to you',
